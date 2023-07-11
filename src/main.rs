00d42cc4622b15a3ae39b3803c8630646862665a
Jacob Shultis <jacob@proteusdev.com>
2023-07-10 23:42:25 -0400

    created image file (2.2)

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    const IMAGE_WIDTH: i32 = 255;
+    const IMAGE_HEIGHT: i32 = 255;
+
+    //std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << '\n255\n';
+
+    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
+
+    for i in (0..IMAGE_HEIGHT - 1).rev() {
+        for j in 0..IMAGE_WIDTH {
+            let r: f32 = (j as f32) / (IMAGE_WIDTH - 1) as f32;
+            let g: f32 = (i as f32) / (IMAGE_HEIGHT - 1) as f32;
+            let b: f32 = 0.25;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            println!("{ir} {ig} {ib}\n");
+        }
+    }
+}
