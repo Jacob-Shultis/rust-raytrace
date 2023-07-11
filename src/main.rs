4a249bf09af2e287ba9494ee1e7633ab884fb96c
Jacob Shultis <jacob@proteusdev.com>
2023-07-11 12:36:43 -0400

    add progress indicator (2.3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 fn main() {
     const IMAGE_WIDTH: i32 = 255;
     const IMAGE_HEIGHT: i32 = 255;
@@ -7,6 +9,10 @@ fn main() {
     println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
 
     for i in (0..IMAGE_HEIGHT - 1).rev() {
+        eprint!("\rScanlines remaining: {i}");
+        
+        std::io::stderr().flush().expect("Unable to flush stderr");
+
         for j in 0..IMAGE_WIDTH {
             let r: f32 = (j as f32) / (IMAGE_WIDTH - 1) as f32;
             let g: f32 = (i as f32) / (IMAGE_HEIGHT - 1) as f32;
@@ -19,4 +25,6 @@ fn main() {
             println!("{ir} {ig} {ib}\n");
         }
     }
+
+    eprintln!("\nDone.\n");
 }
